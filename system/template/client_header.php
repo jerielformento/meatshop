00c85967b865d0f6d058bfdf2b7a03bcfed2e907
program39 <jerielformento@gmail.com>
2022-04-05 18:38:12 +0800

    added url link
    Added site url link to all images path for global setup

--- system/template/client_header.php
+++ system/template/client_header.php
@@ -265,7 +265,7 @@
 	<div class="col-md-12">
 		<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
 			<div class="container">
-				<a class="navbar-brand text-gray-500" href="/"><img class="first-slide" src="images/jl_icon.png" alt="First slide" height=35 width=35></a>
+				<a class="navbar-brand text-gray-500" href="/"><img class="first-slide" src="<?php $this->getSiteUrl(); ?>images/jl_icon.png" alt="First slide" height=35 width=35></a>
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 					<span class="navbar-toggler-icon"></span>
 				</button>
@@ -355,7 +355,7 @@
                       <div class="carousel-caption text-left">
                       <h1 class="text-danger">Frozen Meat</h1>
                       <p>100% Imported beef and delicious!</p>
-                      <p><a class="btn btn-lg btn-danger" href="#" role="button">Shop Now</a></p>
+                      <p><a class="btn btn-lg btn-danger btn-flat" href="#" role="button">Shop Now</a></p>
                       </div>
                   </div>
                   </div>
